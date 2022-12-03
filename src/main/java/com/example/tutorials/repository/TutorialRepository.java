81bbe7586b1d63fd4b6cc775d40b8e1148d50fd5
dongocanh96 <ngocanh6111996@gmail.com>
2022-12-03 17:05:12 +0700

    add controller for app

--- src/main/java/com/example/tutorials/repository/TutorialRepository.java
+++ src/main/java/com/example/tutorials/repository/TutorialRepository.java
@@ -2,9 +2,11 @@ package com.example.tutorials.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.tutorials.model.Tutorial;
 
-public interface TutorialRepository {
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
     List<Tutorial> findByTitleContaining(String title);
 }
