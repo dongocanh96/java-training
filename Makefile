c8e86a66370dcb23f213fd531c86c60f779ece80
dongocanh96 <ngocanh6111996@gmail.com>
2022-12-08 22:09:06 +0700

    add mysql service

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
 setuppostgres:
     docker run --name postgres15 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:15.1-alpine3.17
 
-createdb:
+setupmysql:
+    docker run  --name mysql -e MYSQL_ROOT_PASSWORD=secret -d mysql:latest
+
+setuporacle:
+    docker run --name oracle21 -p 1521:1521 -e ORACLE_PASSWORD=secret -e ORACLE_DATABASE=mydb -e APP_USER=root -e APP_USER_PASSWORD=secret -e STARTUP_NOMOUNT -d gvenzl/oracle-xe:latest
+
+createdbpostgres:
 	docker exec -it postgres15 createdb --username=root --owner=root tutorials
 
-dropdb:
+dropdbpostgres:
 	docker exec -it postgres15 dropdb tutorials
+
+createdbmysql:
+	docker exec -it mysql  mysql -uroot -psecret -e "create database tutorials;"
+
+dropdbmysql:
+	docker exec -it mysql mysql -uroot -psecret -e "drop database tutorials;"
--- src/test/java/com/example/tutorials/TutorialsApplicationTests.java
+++ src/test/java/com/example/tutorials/TutorialsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.tutorials;
 
+import com.example.tutorials.controller.TutorialController;
+import com.example.tutorials.model.Tutorial;
+import com.example.tutorials.repository.TutorialRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class TutorialsApplicationTests {
 
+	@Autowired
+	private TutorialController controller;
+
+	@MockBean
+	private TutorialRepository repository;
+
 	@Test
-	void contextLoads() {
+	public void getTutorialsTest() {
+		when(repository.findAll()).thenReturn(Stream
+				.of(new Tutorial("Spring boot tutorial #7", "Description for tut #7", false),
+				new Tutorial("Spring boot tutorial #8", "Description for tut #8", false))
+				.collect(Collectors.toList()));
+		assertEquals(2, repository.findAll().size());
 	}
 
 }
